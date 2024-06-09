88108f8b38d6e0e469079c08b4f691d7d8876f06
Admin <rj.salazara@uea.edu.ec>
2024-06-09 17:14:59 -0500

    mediante la creación de ejemplos

--- Tecnicas_de_programacion/Ejemplos_de_tecnicas_programacion.py
+++ Tecnicas_de_programacion/Ejemplos_de_tecnicas_programacion.py
@@ -1,102 +1,64 @@
-class Personaje:
-
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
-        self.nombre = nombre
-        self.fuerza = fuerza
-        self.inteligencia = inteligencia
-        self.defensa = defensa
-        self.vida = vida
-
-    def atributos(self):
-        print(self.nombre, ":", sep="")
-        print("·Fuerza:", self.fuerza)
-        print("·Inteligencia:", self.inteligencia)
-        print("·Defensa:", self.defensa)
-        print("·Vida:", self.vida)
-
-    def subir_nivel(self, fuerza, inteligencia, defensa):
-        self.fuerza = self.fuerza + fuerza
-        self.inteligencia = self.inteligencia + inteligencia
-        self.defensa = self.defensa + defensa
-
-    def esta_vivo(self):
-        return self.vida > 0
-
-    def morir(self):
-        self.vida = 0
-        print(self.nombre, "ha muerto")
-
-    def daño(self, enemigo):
-        return self.fuerza - enemigo.defensa
-
-    def atacar(self, enemigo):
-        daño = self.daño(enemigo)
-        enemigo.vida = enemigo.vida - daño
-        print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
-        if enemigo.esta_vivo():
-            print("Vida de", enemigo.nombre, "es", enemigo.vida)
-        else:
-            enemigo.morir()
+# Abstracción: Definimos una clase abstracta Vehiculo
+from abc import ABC, abstractmethod
 
 
-class Guerrero(Personaje):
+class Vehiculo(ABC):
+    def __init__(self, marca, modelo):
+        self._marca = marca  # Encapsulación: Uso de atributos privados
+        self._modelo = modelo
 
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, espada):
-        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
-        self.espada = espada
+    @abstractmethod
+    def arrancar(self):
+        pass
 
-    def cambiar_arma(self):
-        opcion = int(input("Elige un arma: (1) Acero Valyrio, daño 8. (2) Matadragones, daño 10"))
-        if opcion == 1:
-            self.espada = 8
-        elif opcion == 2:
-            self.espada = 10
-        else:
-            print("Número de arma incorrecta")
+    @abstractmethod
+    def detener(self):
+        pass
 
-    def atributos(self):
-        super().atributos()
-        print("·Espada:", self.espada)
-
-    def daño(self, enemigo):
-        return self.fuerza * self.espada - enemigo.defensa
+    def obtener_informacion(self):
+        return f"Marca: {self._marca}, Modelo: {self._modelo}"
 
 
-class Mago(Personaje):
+# Herencia: La clase Coche hereda de Vehiculo
+class Coche(Vehiculo):
+    def __init__(self, marca, modelo, puertas):
+        super().__init__(marca, modelo)
+        self._puertas = puertas  # Encapsulación: Uso de atributos privados
 
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, libro):
-        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
-        self.libro = libro
+    def arrancar(self):
+        return "El coche ha arrancado"
 
-    def atributos(self):
-        super().atributos()
-        print("·Libro:", self.libro)
+    def detener(self):
+        return "El coche se ha detenido"
 
-    def daño(self, enemigo):
-        return self.inteligencia * self.libro - enemigo.defensa
-
-
-def combate(jugador_1, jugador_2):
-    turno = 0
-    while jugador_1.esta_vivo() and jugador_2.esta_vivo():
-        print("\nTurno", turno)
-        print(">>> Acción de ", jugador_1.nombre, ":", sep="")
-        jugador_1.atacar(jugador_2)
-        print(">>> Acción de ", jugador_2.nombre, ":", sep="")
-        jugador_2.atacar(jugador_1)
-        turno = turno + 1
-    if jugador_1.esta_vivo():
-        print("\nHa ganado", jugador_1.nombre)
-    elif jugador_2.esta_vivo():
-        print("\nHa ganado", jugador_2.nombre)
-    else:
-        print("\nEmpate")
+    def obtener_informacion(self):
+        return f"{super().obtener_informacion()}, Puertas: {self._puertas}"
 
 
-personaje_1 = Guerrero("Guts", 20, 10, 4, 100, 4)
-personaje_2 = Mago("Vanessa", 5, 15, 4, 100, 3)
+# Herencia: La clase Moto hereda de Vehiculo
+class Moto(Vehiculo):
+    def __init__(self, marca, modelo, tipo):
+        super().__init__(marca, modelo)
+        self._tipo = tipo  # Encapsulación: Uso de atributos privados
 
-personaje_1.atributos()
-personaje_2.atributos()
+    def arrancar(self):
+        return "La moto ha arrancado"
 
-combate(personaje_1, personaje_2)
+    def detener(self):
+        return "La moto se ha detenido"
+
+    def obtener_informacion(self):
+        return f"{super().obtener_informacion()}, Tipo: {self._tipo}"
+
+
+# Polimorfismo: Usamos una lista de vehículos y llamamos a métodos que se comportan de manera diferente
+vehiculos = [
+    Coche("Toyota", "Corolla", 4),
+    Moto("Harley Davidson", "Sportster", "Cruiser")
+]
+
+for vehiculo in vehiculos:
+    print(vehiculo.arrancar())
+    print(vehiculo.detener())
+    print(vehiculo.obtener_informacion())
+    print("-" * 30)
